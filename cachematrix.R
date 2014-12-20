2f0d3463b5f83c6b5894bdc48a6bf9c7fd35e944
Ken Bell <bitbucket@kbell.fastmail.fm>
2014-12-20 12:21:40 -0500

    corrected minor typos and added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 ## Invert Matrix functions.
 ## The two functions here are used to compute the inverse of a matrix.
 ## First use "MyMatrix <- makeCacheMatrix(your_square_matrix)" to create a function MyMatrix
-## and initialize it with your square matrix.
-## The call "cacheSolve(MyMatrix) to find the inverse on your_square_matrix
+## function named MyMatrix initialized with your square matrix.
+## Then call "cacheSolve(MyMatrix) to find the inverse on your_square_matrix
 
 ## makeCacheMatrix is used to initilize the variables and functions that 
 ## will be used by cacheSolve
@@ -10,7 +10,8 @@ makeCacheMatrix <- function(x = matrix()) {
       ## Initialize m with NULL
       m <- NULL
       
-      ## Set function is Only used for testing to change the initial matrix.  
+      ## The set function is only used for testing and will change the initial matrix with
+      ## a new square matrix specified in y.  
       set <- function(y) {
             ## store the new square matrix in x in the parent function
             x <<- y
@@ -18,30 +19,30 @@ makeCacheMatrix <- function(x = matrix()) {
             m <<- NULL
       }
       
-      ## Returns the initial Matrix to cacheSolve
+      ## Returns the initial Matrix when called by cacheSolve
       get <- function() x
       
-      ## set m with the solved matrix inverse
+      ## sets m with the solved matrix inverse
       setsolved <- function(solved) m <<- solved
       
-      ## returns the matrix inverse
+      ## returns the matrix inverse if cached or returns NULL
       getsolve <- function() m
       
-      ## needed to allow setsolve and getsolve to be called by cacheSolve
+      ## needed to allow set, get, setsolve and getsolve to be callable by cacheSolve
       list(set = set, get = get,
-           setmean = setmean,
+           setsolved = setsolved,
            getsolve = getsolve)
 
 }
 
-## cacheSolve is used to compute the inverse of a matrix.  If this is the first call (i.e. m = NULL)
+## cacheSolve is used to compute the inverse of a matrix.  If this is the first call to cacheSolve (i.e. m = NULL)
 ## then compute inverse and store it in parent function environment for possible future calls.
 cacheSolve <- function(x, ...) {
       ## returns either the already solved inverse matrix or NULL
       m <- x$getsolve()
       
-      ## if m is NULL then the matrix inverse has already been computed so we simply return
-      ## the previously cached data and exit the function
+      ## if m is not NULL then the matrix inverse has already been computed so we simply return
+      ## the previously cached inverse data and exit the function
       if(!is.null(m)) {
             message("getting cached data")
             return(m)
@@ -53,11 +54,11 @@ cacheSolve <- function(x, ...) {
       ## get the initial square matrix
       data <- x$get()
       
-      ## find the matrix inverse using the built-in 'solve' function 
+      ## find the matrix inverse using the built-in 'solve' function and store it in to m
       m <- solve(data, ...)
       
       ## save the solved matrix inverse in the parent function
-      x$setsolve(m)
+      x$setsolved(m)
       
       ## return the solved inverse matrix (for printing?) and exit
       m
